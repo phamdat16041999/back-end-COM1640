faad0136b02ca7fb98b3f5da3ec62c942cf12d32
buidinhkha <57929886+buidinhkha@users.noreply.github.com>
2021-03-05 15:16:30 +0700

    a

--- Student/forms.py
+++ Student/forms.py
@@ -4,8 +4,4 @@ from Login.models import Data
 class DataForm(forms.ModelForm):
 	class Meta:
 		model = Data
-<<<<<<< HEAD
 		fields = ('idData','Data')
-=======
-		fields = ('idData','Data', 'Contribute')
->>>>>>> aecc2e6aa0344185093971c2eef33086382042f6
