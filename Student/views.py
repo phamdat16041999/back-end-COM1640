26b97f33eca05fcd6dd0562e5dac635dc96cfc2c
Mr.Victory1509 <55972167+mrvictory1509@users.noreply.github.com>
2021-03-24 10:46:08 +0700

    a

--- Student/views.py
+++ Student/views.py
@@ -240,6 +240,47 @@ def uploadContribute(request,id):
             return redirect('/')
     else:
         return render(request, 'login.html')
+def updateContribute(request,id):
+    if request.user.is_authenticated and getAuthGroup(request.user.id) == "Student":
+        if request.method == 'POST' and request.FILES['contribute'] and request.FILES['image1'] and request.FILES['image2']:
+            nameContribute = request.POST.get('nameContribute','')
+            description = request.POST.get('description','')
+            contribute = request.FILES['contribute']
+            image1 = request.FILES['image1']
+            image2 = request.FILES['image2']
+            Contribute.objects.filter(TermID_id = id, UserID_id = request.user.id).delete()
+            Contribute.objects.create(Name = nameContribute, Description = description, TermID_id = id, Status = False, UserID_id = request.user.id, Document = contribute, Readed = False)
+            Data.objects.create(Data = image1, ContributeID_id = Contribute.objects.latest('id').id)
+            Data.objects.create(Data = image2, ContributeID_id = Contribute.objects.latest('id').id)
+            # Send email
+            facultyID = User.objects.filter(id = request.user.id)[0].Faculty_id
+            facultyName = Faculty.objects.filter(id = facultyID)[0].Name
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT login_user.email FROM login_user INNER JOIN login_user_groups ON login_user.id = login_user_groups.user_id INNER JOIN auth_group ON login_user_groups.group_id = auth_group.id INNER JOIN login_faculty ON login_faculty.id = login_user.Faculty_id WHERE auth_group.name = %s AND login_faculty.Name = %s" ,
+                    ["Coordinator",facultyName]
+                )
+                Email = cursor.fetchall()
+            
+            for i in Email:
+                CLIENT_SECRET_FILE = './client_secret.json'
+                API_NAME = "gmail"
+                API_VERSION = "v1"
+                SCOPES = ["https://mail.google.com/"]
+                service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+                emailMsg = "The student whose name is "+ User.objects.filter(id = request.user.id)[0].last_name+" contributes a magazine to the term "+Term.objects.filter(idTerm=id)[0].NameTerm+".\n Please visit here to see contributions: http://localhost:8000/Coordinator/viewContribute/" +  str(Contribute.objects.latest('id').id)
+                mimeMessage = MIMEMultipart()
+                mimeMessage["to"] = i[0]
+                mimeMessage["subject"] = "Notification"
+                mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+                raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+                message = service.users().messages().send(userId="me", body={"raw": raw_string}).execute()
+            return redirect('/Student/ViewDeadline')
+        else:
+            # Not found 404
+            return redirect('/')
+    else:
+        return render(request, 'login.html')
 def sendMessenger(request, id, messenger):
     if request.user.is_authenticated and getAuthGroup(request.user.id) == "Student":
         ContributeID = Contribute.objects.get(TermID = id, UserID = request.user.id)
